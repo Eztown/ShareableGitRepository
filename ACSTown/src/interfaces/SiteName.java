4b90b3b48e7969702af99279b6ee35c46032ed2b
Maple <Maple@10.9.147.179>
2017-01-23 13:00:21 -0600

    I fixed the lab.

--- ACSTown/src/interfaces/SiteName.java
+++ ACSTown/src/interfaces/SiteName.java
@@ -9,18 +9,23 @@ import static java.lang.System.*;
 
 class SiteName implements Comparable<SiteName>
 {
-	String name;
+	private String name, end;
 
 	public SiteName(String n){
 		name = n;
+		end = n.substring(n.indexOf('.')+1);
 	}
 	
 	String getName(){
 		return name;
 	}
 	
+	String getEnd(){
+		return end;
+	}
+	
 	public int compareTo(SiteName arg0) {
-		return name.compareTo(arg0.getName());
+		return end.equals(arg0.getName())?name.compareTo(arg0.getName()):end.compareTo(arg0.getEnd());
 	}
 	
 	public String toString(){
